7ed6bb5d63d6ad0d0716eb49830a8bb41d7d9133
Ivan Yeremenko <i.p.yeremenko@gmail.com>
2024-04-02 20:57:36 +0200

    Removed unnecessary files

--- src/options.js
+++
@@ -1,5 +0,0 @@
-export const options = [
-    {value: 'not played yet', label: 'Not played yet'},
-    {value: 'currently playing', label: 'Currently playing'},
-    {value: 'used to play', label: 'Used to play'}
-]
