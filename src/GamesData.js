7c8583f6875ea2f87a3e0de92d9e701c8c649010
Ivan Yeremenko <i.p.yeremenko@gmail.com>
2024-04-02 16:03:39 +0200

    Refactor of the images, fixed

--- src/GamesData.js
+++ src/GamesData.js
@@ -1,12 +1,13 @@
-// import hollowKnight from './img/hollow_knight.jpg';
-// import warcraft from './img/warcraft3.jpg';
-// import genshinImpact from './img/genshin_impact.jpg';
-// import terraria from './img/terraria.png'
+import hollowKnight from './img/hollow_knight.jpg';
+import warcraft from './img/warcraft3.jpg';
+import genshinImpact from './img/genshin_impact.jpg';
+import terraria from './img/terraria.png'
+
 const games = [
     {
         id: 0,
         title: 'Hollow Knight',
-        src: '/img/hollow_knight.jpg',
+        src: hollowKnight,
         description: 'Forge your own path in Hollow Knight! An epic action adventure through a vast ruined kingdom' +
             'of insects and heroes. Explore twisting caverns, battle tainted creatures and befriend bizarre bugs,' +
             'all in a classic, hand-drawn 2D style.',
@@ -16,7 +17,7 @@ const games = [
     {
         id: 1,
         title: 'Genshin Impact',
-        src: '/img/genshin_impact.jpg',
+        src: genshinImpact,
         description: 'Explore a fantasy world called Teyvat, meet beautiful and breathtaking characters to travel with you' +
             'and discover more parts of the world overwhelming with new friends, treasures and stories!',
         genres: 'Action, Anime, Adventure, RPG, Gacha',
@@ -25,7 +26,7 @@ const games = [
     {
         id: 2,
         title: 'Warcraft 3',
-        src: '/img/warcraft3.jpg',
+        src: warcraft,
         description: 'A stunning revolutionary real-time strategy game that laid the foundation for Azeroth’s most epic stories. It is a really cool story. Command the Night Elves, Undead, Orcs, and Humans as alliances shift and armies clash in this timeless real-time strategy game.',
         genres: 'Real-time strategy',
         type: 'Singleplayer'
@@ -33,7 +34,7 @@ const games = [
     {
         id: 3,
         title: 'Terraria',
-        src: '/img/terraria.png',
+        src: terraria,
         description: 'Dig, fight, explore, build! Nothing is impossible in this action-packed adventure game. The world is your canvas and the ground itself is your paint.',
         genres: 'Sandbox, Action, Adventure, RPG',
         type: 'Singleplayer'
